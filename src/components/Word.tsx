7f5696b2c91e377779471a5fb4094f7b30b19441
hisamatsu yoshiyuki <hisasann25@gmail.com>
2023-08-18 17:21:23 +0900

    feat: remove unuse code

--- src/components/Word.tsx
+++ src/components/Word.tsx
@@ -8,14 +8,6 @@ type WordProps = {
 
 const Word: React.FC<WordProps> = (...props) => {
   const [text, setText] = useState('');
-  const textRef = useRef<HTMLInputElement>(null);
-
-  // useEffect(() => {
-  //   // https://qiita.com/akifumii/items/539e4af7ed4d068c0144
-  //   setTimeout(() => {
-  //     textRef.current?.focus();
-  //   }, 0);
-  // }, []);
 
   const inputHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
     // https://stackoverflow.com/questions/59573672/how-do-i-type-e-key-and-e-target-value-with-typescript-in-react
@@ -26,7 +18,7 @@ const Word: React.FC<WordProps> = (...props) => {
   return (
     <>
       <div>
-        <input type="text" ref={textRef} className='word' onInput={inputHandler} autoFocus />
+        <input type="text" className='word' onInput={inputHandler} autoFocus />
         <p><span className='display'>{text}</span></p>
       </div>
       <style jsx="true">{`
